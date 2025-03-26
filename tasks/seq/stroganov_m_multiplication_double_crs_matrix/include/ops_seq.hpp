90166e5fbcb93684c5fb96216b7446d5918d8f12
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-03-26 18:01:37 +0300

    6

--- tasks/seq/stroganov_m_multiplication_double_crs_matrix/include/ops_seq.hpp
+++ tasks/seq/stroganov_m_multiplication_double_crs_matrix/include/ops_seq.hpp
@@ -29,4 +29,4 @@ void MakeCRS(std::vector<unsigned int> &r_i, std::vector<unsigned int> &col, std
 void MatrixMultiplication(const std::vector<double> &a, const std::vector<double> &b, std::vector<double> &c,
                           unsigned int m, unsigned int n, unsigned int p);
 
-}  // stroganov_m_multiplication_double_crs_matrix_seq
+                                                                                                             }  // stroganov_m_multiplication_double_crs_matrix_seq
