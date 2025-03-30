c4896e8f1517090fc3fc6860e5696e32bfe49e4f
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-03-30 22:30:12 +0300

    25

--- tasks/seq/stroganov_m_multiplication_double_crs_matrix/src/ops_seq.cpp
+++ tasks/seq/stroganov_m_multiplication_double_crs_matrix/src/ops_seq.cpp
@@ -39,7 +39,6 @@ bool stroganov_m_multiplication_double_crs_matrix_seq::MuitiplicationCrsMatrixSe
 }
 
 bool stroganov_m_multiplication_double_crs_matrix_seq::MuitiplicationCrsMatrixSeq::ValidationImpl() {
-  /*
   if (task_data->inputs_count[1] != task_data->inputs_count[2]) {
     return false;
   }
@@ -62,13 +61,14 @@ bool stroganov_m_multiplication_double_crs_matrix_seq::MuitiplicationCrsMatrixSe
     return false;
   }
   return true;
-  */
+  /*
   return task_data->inputs_count[1] == task_data->inputs_count[2] &&
          task_data->inputs_count[4] == task_data->inputs_count[5] &&
          task_data->inputs_count[0] == task_data->outputs_count[0] &&
          *std::max_element(reinterpret_cast<unsigned int *>(task_data->inputs[1]),
                            reinterpret_cast<unsigned int *>(task_data->inputs[1]) + task_data->inputs_count[1]) <=
              task_data->inputs_count[3] - 2;
+  */
 }
 
 /*
